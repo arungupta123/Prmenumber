74b529024632953b9fd310a0ceca448a57e7d3b7
ARUN <arun82870@gmail.com>
2023-06-17 17:56:05 +0530

    my first commit

---
+++ src/test/java/BOA/Testing/BOASecond/Driverlaunch.java
@@ -0,0 +1,14 @@
+package BOA.Testing.BOASecond;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driverlaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+}
